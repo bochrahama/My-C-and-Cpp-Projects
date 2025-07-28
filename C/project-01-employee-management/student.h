a86c0535c8745e2cb5eeab1e3009969b90fd4c11
Bochra <Bochrahiba@outlook.com>
2025-07-28 09:07:44 +0200

    Put Function Prototypes in a student.h file

--- C/project-01-employee-management/student.h
+++ C/project-01-employee-management/student.h
@@ -13,5 +13,11 @@ typedef struct {
 
 void add_student(Student students[], int *count);
 void print_students(Student students[], int count);
+FILE* open_file(const char *filename, const char *mode);
+int read_students(Student students[]);
+void sorted_students (Student students[], int cont);
+void save_sorted_students (Student students[], int count );
+void passed_students (Student students[], int count);
+void save_by_Gender (Student students[], int count );
 
 #endif
